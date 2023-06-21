2230641dcda3ab8c24a3e454fb64afa6c10940f4
Irina <irina.blumenfeld@netlify.com>
2023-06-21 17:57:17 -0400

    Add Shopify Data

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Irina Blumenfeld';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Netlify Connect Demo';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
